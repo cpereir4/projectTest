474f6dd7cc0723e1283756e3eb6aedb966d50b21
cpereir4 <>
2021-05-07 09:24:39 -0700

    test

--- Project Prueba/Tables/dbo.C_Item.sql
+++ Project Prueba/Tables/dbo.C_Item.sql
@@ -7,7 +7,8 @@ CREATE TABLE [dbo].[C_Item] (
 		[i_im_id]     [int] NULL,
 		[i_name]      [char](24) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[i_price]     [numeric](5, 2) NULL,
-		[i_data]      [char](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[i_data]      [char](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[i_dated]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 CREATE UNIQUE CLUSTERED INDEX [C_Item_I1]
